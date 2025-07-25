0862d4d1a675c9428216ce8fe930d5e30111791c
Angad Thakur <angadthakur2004@gmail.com>
2025-07-25 17:34:26 +0530

    updated translation logic

--- lib/features/Text_translation/screen/text_translation.dart
+++ lib/features/Text_translation/screen/text_translation.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:translator_app_polyglot/core/api/translation_service.dart';
 import 'package:translator_app_polyglot/features/Text_translation/widgets/input_text_field.dart';
 import 'package:translator_app_polyglot/features/Text_translation/widgets/translated_text_field.dart';
 import 'package:translator_app_polyglot/features/presentation/screens/homescreen.dart';
 
-class TextTranslation extends StatelessWidget {
+class TextTranslation extends StatefulWidget {
   const TextTranslation({super.key});
+
+  @override
+  State<TextTranslation> createState() => _TextTranslationState();
+}
+
+class _TextTranslationState extends State<TextTranslation> {
+  //STATE MANAGEMENT 
+  final TranslationService _translationService = TranslationService();
+
+  final TextEditingController _textController = TextEditingController();
+
+  String _translatedText = "Translation will appear here";
+
+  bool _isLoading = false;
+
+  @override
+  void dispose(){
+     //Clean up the controller when the screen is closed
+     _textController.dispose();
+     super.dispose();
+  }
+
+  //TRANSLATION LOGIC
+  Future <void> _onTranslatePressed() async{
+    if(_textController.text.trim().isEmpty){
+      return;
+    }
+
+    FocusScope.of(context).unfocus(); //To hide the keyboard
+
+    setState(() {
+      _isLoading = true; // to show the loading indicator 
+    });
+
+    try{
+      final result = await _translationService.translate(
+        _textController.text,
+        "es",
+      );
+      setState(() {
+        _translatedText = result;
+      });
+
+    }catch(e){
+      setState(() {
+        _translatedText ="Error: Could not translate.";
+      });
+      print(e);
+    }finally{
+      setState(() {
+        _isLoading = false; // to hide the loading indicator , regardless of success or error
+      });
+    }
+  }
+
+  //Text-to-speech Logic 
+  void _onListenPressed(){
+    //TODO
+    print("Listen button tapped. Text: $_translatedText");
+  }
+
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +85,9 @@ class TextTranslation extends StatelessWidget {
         elevation: 0,
         leading:IconButton(
           onPressed: (){
-          //Takes back to the Home Screen
-          Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const Homescreen()));
+            Navigator.of(context).pop();
+          
+          
 
         print("Back to Home Screen");
           
@@ -58,16 +126,15 @@ class TextTranslation extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children:[
-              const Expanded(
-                child:InputTextField() 
+              Expanded(
+                child:InputTextField(controller: _textController), 
                 ),
 
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: ElevatedButton.icon(onPressed: (){
-                    //TODO : Add translation logic 
-                    print("Translate button tapped!");
-                  }, 
+                  child: _isLoading
+                  ? const CircularProgressIndicator(color: Colors.white,)
+                  : ElevatedButton.icon(onPressed: _onTranslatePressed, 
                   icon: const Icon(Icons.translate),
                   label: const Text("Translate"),
                   style: ElevatedButton.styleFrom(
@@ -79,10 +146,13 @@ class TextTranslation extends StatelessWidget {
                   ), 
                 ),
 
-                const Expanded(
+                Expanded(
                   child: TranslatedTextField(
-                    translatedText:"This is where the translated text will appear after you press")
+                   translatedText: _translatedText,
+                   targetLanguage: "Spanish",
+                   onListenPressed: _onListenPressed,
                   )
+                )
               
             ],
           )),
