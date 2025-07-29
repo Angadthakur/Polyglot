3a56a0d1a61f0c6d0ec10c7b217bd277dedfb53c
Angad Thakur <angadthakur2004@gmail.com>
2025-07-29 12:41:06 +0530

    project utils folder

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:translator_app_polyglot/core/widgets/language_selector.dart';
+
+final List<Language> supportedLanguages = const [
+    Language(name: 'English', code: 'en'),
+    Language(name: 'Spanish', code: 'es'),
+    Language(name: 'French', code: 'fr'),
+    Language(name: 'German', code: 'de'),
+    Language(name: 'Japanese', code: 'ja'),
+    Language(name: 'Hindi', code: 'hi'),
+    Language(name: 'Russian', code: 'ru'),
+  ];
